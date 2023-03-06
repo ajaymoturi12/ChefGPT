5d86799e11fcf9439e157a57b943cad1dfb8f0c2
ajaymoturi12 <ajaymoturi0@gmail.com>
2023-03-06 16:12:47 -0500

    init

---
+++ ChefGPT/ChefGPT/ChefGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefGPTApp.swift
+//  ChefGPT
+//
+//  Created by Ajay Moturi on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ChefGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
