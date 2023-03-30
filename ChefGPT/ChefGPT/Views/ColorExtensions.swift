7ccf0cc0b2f446c39a055eef70591300c1a2670d
iWatger <ethanfox646@icloud.com>
2023-03-30 18:05:53 -0400

    added some silly colors bc im silly

---
+++ ChefGPT/ChefGPT/Views/ColorExtensions.swift
@@ -0,0 +1,23 @@
+//
+//  ColorExtensions.swift
+//  ChefGPT
+//
+//  Created by Ethan Fox on 3/30/23.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Color {
+    
+    public static func GPTlightGrey() -> Color {
+        Color.init(red: 214/255, green: 214/255, blue: 214/255)
+    }
+    
+    public static func GPTorange() -> Color {
+        Color.init(red: 237/255, green: 104/255, blue: 46/255)
+    }
+    
+    
+}
--- ChefGPT/ChefGPT/Views/Pantry Tab Views/IndividualIngredientView.swift
+++ ChefGPT/ChefGPT/Views/Pantry Tab Views/IndividualIngredientView.swift
@@ -14,7 +14,7 @@ struct IndividualIngredientView: View {
     var body: some View {
         ZStack {
             Rectangle()
-                .foregroundColor(.gray)
+                .foregroundColor(.lightGrey())
             HStack() {
                 HStack {
                     Image(systemName: "leaf.circle")
