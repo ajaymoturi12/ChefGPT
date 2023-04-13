64316e59014a53f414dd976671b94bed1fc8bca6
iWatger <ethanfox646@icloud.com>
2023-04-13 18:14:26 -0400

    removed code for button and camera sheet presentation

--- ChefGPT/ChefGPT/Views/Pantry Tab Views/PantryView.swift
+++ ChefGPT/ChefGPT/Views/Pantry Tab Views/PantryView.swift
@@ -10,9 +10,6 @@ import SwiftUI
 struct PantryView: View {
     @EnvironmentObject var model: Model
     
-    @State private var showingImagePicker = false
-    @State private var inputImages:[UIImage] = [UIImage]()
-    
     var body: some View {
         ZStack {
             VStack {
@@ -33,27 +30,8 @@ struct PantryView: View {
                 }
             }
 
-            //Button stuff overlay
-            VStack {
-                Spacer()
-                HStack {
-                    Spacer()
-                    // This is the button that someone presses to add new ingredients 
-                    Button {
-                        showingImagePicker = true
-                    } label: {
-                        Image(systemName: "plus.circle.fill")
-                            .resizable()
-                            .frame(width: 70, height: 70)
-                            .foregroundColor(.GPTorange())
-                    }
-                }
-            }
         }
         .padding()
-        .fullScreenCover(isPresented: $showingImagePicker) {
-            CameraView()
-        }
     }
 }
 struct PantryView_Previews: PreviewProvider {
