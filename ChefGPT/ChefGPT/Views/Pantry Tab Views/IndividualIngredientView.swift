624f1aea1560c00990bd2bdb42a08cdc3c429d0c
iWatger <ethanfox646@icloud.com>
2023-04-15 13:18:37 -0400

    refactored some of the IndividualIngredientView.swift and removed the leaf image

--- ChefGPT/ChefGPT/Views/Pantry Tab Views/IndividualIngredientView.swift
+++ ChefGPT/ChefGPT/Views/Pantry Tab Views/IndividualIngredientView.swift
@@ -12,52 +12,48 @@ struct IndividualIngredientView: View {
     @State var ingredient: ExtendedIngredients
     
     var body: some View {
-        ZStack {
-            Rectangle()
-                .foregroundColor(.GPTlightGrey())
-            HStack() {
-                HStack {
-                    Image(systemName: "leaf.circle")
+        HStack {
+            HStack {
+                Text(ingredient.name)
+                    .font(.title2)
+                    .bold()
+                Spacer()
+                Button {
+                    if ingredient.amount > 0 {
+                        ingredient.amount -= 1
+                    }
+                } label: {
+                    Image(systemName: "minus.circle.fill")
                         .resizable()
-                        .frame(width: 50, height: 50)
-//                        .padding(25)
-                    Text(ingredient.name)
-                    Spacer()
-                    Button {
-                        if ingredient.amount > 0 {
-                            ingredient.amount -= 1
-                        }
-                    } label: {
-                        Image(systemName: "minus.circle.fill")
-                            .resizable()
-                            .frame(width: 20, height: 20)
-                            .foregroundColor(.black)
-                    }
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(.black)
+                }
+                
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(.white)
                     
-                    ZStack {
-                        Rectangle()
-                            .foregroundColor(.white)
-                        
-                        Text(String(format: "%.1f", ingredient.amount) + " \(ingredient.unit)")
-                    }
-                    
-                    .frame(width:100)
-                    .cornerRadius(10)
-                    Button {
-                        ingredient.amount += 1
-                    } label: {
-                        Image(systemName: "plus.circle.fill")
-                            .resizable()
-                            .frame(width: 20, height: 20)
-                            .foregroundColor(.black)
-                    }
+                    Text(String(format: "%.1f", ingredient.amount) + " \(ingredient.unit)")
+                }
+                
+                .frame(width:100)
+                .cornerRadius(10)
+                Button {
+                    ingredient.amount += 1
+                } label: {
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(.black)
                 }
             }
-            .padding()
-            
         }
-        .frame(width: .infinity, height: 70)
-        .cornerRadius(10)
+        .font(.system(.body, design: .rounded, weight: .regular))
+        .padding()
+        .frame(maxHeight: 80)
+        .background(Color.GPTlightGrey())
+        .cornerRadius(15)
+        
     }
 }
 
